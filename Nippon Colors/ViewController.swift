92f3f485aef5348559ee666a336cb456452f89b2
yuyang <yy@mailizc.com>
2018-10-26 14:08:43 +0800

    Initial Commit

---
+++ Nippon Colors/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Nippon Colors
+//
+//  Created by 好多余先生 on 2018/10/26.
+//  Copyright © 2018 好多余先生. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
